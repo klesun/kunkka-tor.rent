544dff9355422f3665c217cac5261de5d4462956
Artur Klesun ECMA God <arturklesun@gmail.com>
2020-10-29 01:55:07 +0200

    Add public APIs to get torrent seeds and meta data by hash

--- src/server/HandleHttpRequest.ts
+++ src/server/HandleHttpRequest.ts
@@ -4,6 +4,7 @@ import * as http from "http";
 import * as parseTorrent from 'parse-torrent';
 
 const fs = fsSync.promises;
+const torrentStream = require('torrent-stream');
 
 const Rej = require('klesun-node-tools/src/Rej.js');
 const {getMimeByExt, removeDots, setCorsHeaders} = require('klesun-node-tools/src/Utils/HttpUtil.js');
@@ -82,6 +83,56 @@ const serveStaticFile = async (pathname: string, params: HandleHttpParams) => {
     }
 };
 
+const limitTime = <T>(maxSeconds: number, promise: Promise<T>): Promise<T> => {
+    return new Promise((resolve, reject) => {
+        let resolved = false;
+        let rejected = false;
+        promise.then(result => {
+            if (!rejected) {
+                resolved = true;
+                resolve(result);
+            }
+        });
+        setTimeout(() => {
+            if (!resolved) {
+                rejected = true;
+                const msg = 'Promise timed out after ' + maxSeconds + ' seconds';
+                reject(new Error(msg));
+            }
+        }, maxSeconds * 1000);
+    });
+};
+
+const getCircularReplacer = () => {
+    const seen = new WeakSet();
+    return (key, value) => {
+        if (typeof value === "object" && value !== null) {
+            if (seen.has(value)) {
+                return;
+            }
+            seen.add(value);
+        }
+        return value;
+    };
+};
+
+const makeSwarmSummary = (swarm) => {
+    let seederWires = 0;
+    let otherWires = 0;
+    for (const wire of swarm.wires) {
+        if (wire.isSeeder) {
+            ++seederWires;
+        } else {
+            ++otherWires;
+        }
+    }
+    return {
+        seederWires: seederWires,
+        otherWires: otherWires,
+        peers: Object.keys(swarm._peers).length,
+    };
+};
+
 const Api = {
     routes: {
         '/api/testTorrentInfo': async rq => {
@@ -89,7 +140,55 @@ const Api = {
             return parseTorrent(magnetLink);
         },
         '/api/testTorrentStream': async rq => {
+        },
+        '/api/checkInfoHashMeta': async rq => {
+            const {infoHash} = url.parse(rq.url, true).query;
+            if (!infoHash || infoHash.length !== 40) {
+                throw new Error('Invalid infoHash, must be a 40 characters long hex string');
+            }
 
+            const engine = torrentStream('magnet:?xt=urn:btih:' + infoHash);
+            const whenMeta = new Promise(resolve => {
+                engine.on('torrent', async (torrent) => {
+                    resolve({
+                        name: torrent.name,
+                        length: torrent.length,
+                        files: torrent.files.map(f => ({
+                            path: f.path, length: f.length,
+                        })),
+                    });
+                });
+            });
+            return limitTime(30, whenMeta)
+                .finally(() => engine.destroy());
+        },
+        '/api/checkInfoHashPeers': async rq => {
+            const {infoHash} = url.parse(rq.url, true).query;
+            if (!infoHash || infoHash.length !== 40) {
+                throw new Error('Invalid infoHash, must be a 40 characters long hex string');
+            }
+            const engine = torrentStream('magnet:?xt=urn:btih:' + infoHash);
+            // would be real cool to send response incrementally...
+            engine.listen();
+            await new Promise(resolve => setTimeout(resolve, 5 * 1000));
+            const summary = makeSwarmSummary(engine.swarm);
+            engine.destroy();
+
+            // {"delay":1,"summary":{"seederWires":0,"otherWires":0,"wires":0,"peers":0}},
+            // {"delay":5,"summary":{"seederWires":7,"otherWires":2,"wires":9,"peers":48}},
+            // {"delay":10,"summary":{"seederWires":7,"otherWires":2,"wires":9,"peers":20}},
+            // {"delay":20,"summary":{"seederWires":7,"otherWires":2,"wires":9,"peers":20}},
+            // {"delay":40,"summary":{"seederWires":8,"otherWires":2,"wires":10,"peers":20}}
+            // const measurements = [];
+            // const delays = [1, 5, 10, 20, 40];
+            // for (const delay of delays) {
+            //     await new Promise(resolve => setTimeout(resolve, delay * 1000));
+            //     const summary = makeSwarmSummary(engine.swarm);
+            //     measurements.push({delay, summary});
+            // }
+
+            engine.destroy();
+            return summary;
         },
     },
 };
