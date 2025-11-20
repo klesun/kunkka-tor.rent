c478aaa9900e0ffc680d082c56108d4ad8c0b83d
Artur Klesun <arturklesun@gmail.com>
2025-11-20 10:09:35 +0200

    Correct spacing in an import

--- src/server/Api.ts
+++ src/server/Api.ts
@@ -13,7 +13,7 @@ const util = require("util");
 const execFile = util.promisify(require("child_process").execFile);
 import * as fs from "fs";
 import * as parseTorrent from "parse-torrent";
-import {BadGateway, BadRequest, NotFound, NotImplemented, TooEarly} from "@curveball/http-errors";
+import { BadGateway, BadRequest, NotFound, NotImplemented, TooEarly } from "@curveball/http-errors";
 import TorrentNamesFts from "./repositories/TorrentNamesFts.js";
 import { trackerRecords } from "./actions/ScrapeTrackersSeedInfo.js";
 import Infohashes from "./repositories/Infohashes.js";
